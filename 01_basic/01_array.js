9dc37d838b95cd61a45ccf1f1a242585b7dfe942
Sumant sahoo <163635318+Sumant246u@users.noreply.github.com>
2025-03-05 13:24:49 +0000

    Array push,pop,unshift,shift,includes,indexOf,slice,splice

---
+++ 01_basic/01_array.js
@@ -0,0 +1,39 @@
+//Array
+
+const myArr= [0,1,2,3,4,5,6,7,8]
+// console.log(myArr[4]);
+
+//Array methods
+
+// myArr.push(6)
+// myArr.pop(6)
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(4));
+
+const newArr=myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+//slice, splice
+
+
+const myn1=myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2=myArr.splice(1,3)
+console.log("c",myArr);
+console.log(myn2);
+
+
+
+
+
+
